7bcdc01e3d20c2e41cb5315403cac8a8dd127951
Chvvn17 <minh-chien.luong@hotmail.com>
2025-08-31 19:09:36 +0200

    Resolve Seeds

--- guess.go
+++ guess.go
@@ -8,12 +8,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 )
 
 func main() {
-	seconds := time.Now().Unix()
-	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
